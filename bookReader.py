94ee68ee5176d5f7d65ed42b3c6c957f45dae091
Akshaygothe <52615789+Akshaygothe@users.noreply.github.com>
2021-04-03 07:56:47 -0700

    Add files via upload
    bookReader basic program which read books for you. Just you have to upload book in that forlder.

---
+++ bookReader.py
@@ -0,0 +1,67 @@
+import os
+import PyPDF2
+import pyttsx3
+import fitz 
+import sys 
+from pynput import keyboard
+import keyboard
+import sys
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+dir_path = 'F:/Books/'
+books = ['A brief history of human kind']
+#path = r'C:/Users/Admini/newproj/A-Very-Short-Story.pdf'
+
+def bookReader(path, book_name):
+    get_out = 0
+    doc = fitz.open(path)
+    pdf_size = doc.pageCount
+    print("number of pages:",doc.pageCount)
+    page_no = int(input("Enter the page number from you want to listen: "))
+    for i in range(page_no, pdf_size):
+        pageText = doc.loadPage(i)
+        pageText = pageText.getText("text")
+        pageText = pageText.replace('\r', '').replace('\n', '')
+        sent_list = pageText.split('.')
+        print(sent_list)
+        for sent in sent_list:
+            sent = sent.strip()
+            if keyboard.is_pressed('q'): 
+                speak('You Stop reading Book!')
+                get_out = 1
+                break
+            if keyboard.is_pressed('n'):
+                break
+            speak(sent)
+        if get_out == 1:
+            break
+
+if __name__ == '__main__':
+    print("Welcome to Book Reader")
+    print("Avalibal Book List:")
+    for count, book in enumerate(books):
+        print(count, book)
+    
+    book_number = int(input("Which Book You want To Listen: "))
+    sucess = bookReader(os.path.join(dir_path + books[book_number] + '.pdf'), books[book_number])
+    print("visit Next Time.")
+
+
+
+    
+
+
+
+
+
+
+
+
+
