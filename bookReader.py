c201b2425c577c7cff004af8f249475b566d6945
Akshaygothe <52615789+Akshaygothe@users.noreply.github.com>
2021-04-03 08:07:01 -0700

    Update bookReader.py

--- bookReader.py
+++ bookReader.py
@@ -25,6 +25,7 @@ def bookReader(path, book_name):
     pdf_size = doc.pageCount
     print("number of pages:",doc.pageCount)
     page_no = int(input("Enter the page number from you want to listen: "))
+    exited_page = 0
     for i in range(page_no, pdf_size):
         pageText = doc.loadPage(i)
         pageText = pageText.getText("text")
@@ -36,12 +37,14 @@ def bookReader(path, book_name):
             if keyboard.is_pressed('q'): 
                 speak('You Stop reading Book!')
                 get_out = 1
+                exited_page = i
                 break
             if keyboard.is_pressed('n'):
                 break
             speak(sent)
         if get_out == 1:
             break
+    return exited_page 
 
 if __name__ == '__main__':
     print("Welcome to Book Reader")
@@ -50,7 +53,9 @@ if __name__ == '__main__':
         print(count, book)
     
     book_number = int(input("Which Book You want To Listen: "))
-    sucess = bookReader(os.path.join(dir_path + books[book_number] + '.pdf'), books[book_number])
+    exited_page = bookReader(os.path.join(dir_path + books[book_number] + '.pdf'), books[book_number])
+    print(f'You stop on page Number: {exited_page}')
+    print("remember it for next time.")
     print("visit Next Time.")
 
 
